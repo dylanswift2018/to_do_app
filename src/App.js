cb6d3908a04efc9252de9f121049a1f503f8404b
Lotfi Feraga <hl_feraga@esi.dz>
2021-11-09 13:35:02 +0000

    added the components and the overall structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import User from './components/User'
+import Calendar from './components/Calendar'
+import Projects from './components/Projects'
+import AddNewTodo from './components/AddNewTodo'
+import Todos from './components/Todos'
+import EditTodo from './components/EditTodo'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Header>
+         <User />
+         <AddNewTodo />
+         <Calendar />
+         <Projects />
+      </Header>
+      <Main>
+        <Todos />
+        <EditTodo />
+
+      </Main>
     </div>
   );
 }
